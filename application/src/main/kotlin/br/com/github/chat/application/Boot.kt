85c8786f07bf75946f3660986972c01a707ac549
Rafael Martins <rafaeltecinfo9@gmail.com>
2022-04-10 23:09:23 -0300

    Adding component scan spring annotation

--- application/src/main/kotlin/br/com/github/chat/application/Boot.kt
+++ application/src/main/kotlin/br/com/github/chat/application/Boot.kt
@@ -2,8 +2,10 @@ package br.com.github.chat.application
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["br.com.github.chat.*"])
 class Boot
 
 fun main() {
