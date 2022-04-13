fdf96df40b8195c83ad3e192cce74816ffc1d948
Rafael Martins <rafaeltecinfo9@gmail.com>
2022-04-12 22:20:20 -0300

    Creating user candidate request model

--- rest/src/main/kotlin/br/com/github/chat/rest/request/UserCandidateRequest.kt
+++ rest/src/main/kotlin/br/com/github/chat/rest/request/UserCandidateRequest.kt
@@ -1,17 +1,34 @@
 package br.com.github.chat.rest.request
 
+import java.time.LocalDate
 import java.time.LocalDateTime
 import javax.validation.constraints.NotNull
 
 data class UserCandidateRequest(
     @NotNull
-    val birthDate: LocalDateTime,
+    val person: PersonRequest,
 
+    @NotNull
+    val phoneNumber: PhoneNumberRequest,
+
+    @NotNull
+    val device: DeviceRequest
+)
+
+data class PersonRequest(
     @NotNull
     val name: String,
 
     @NotNull
-    val phoneNumber: PhoneNumberRequest
+    val birthDate: LocalDate
+)
+
+data class DeviceRequest(
+    @NotNull
+    val manufacturer: String,
+
+    @NotNull
+    val system: SystemRequest
 )
 
 data class PhoneNumberRequest(
@@ -23,4 +40,12 @@ data class PhoneNumberRequest(
 
     @NotNull
     val number: String
+)
+
+data class SystemRequest(
+    @NotNull
+    val version: String,
+
+    @NotNull
+    val systemOperation: String,
 )
