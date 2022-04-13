93b7d7e210df09581103c42561bca69bb11bee0f
Rafael Martins <rafaeltecinfo9@gmail.com>
2022-04-12 22:19:55 -0300

    Creating user controller for endpoint /v1/user

--- rest/src/main/kotlin/br/com/github/chat/rest/controller/UserController.kt
+++ rest/src/main/kotlin/br/com/github/chat/rest/controller/UserController.kt
@@ -1,7 +1,7 @@
 package br.com.github.chat.rest.controller
 
 import br.com.github.chat.rest.request.UserCandidateRequest
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,12 +11,12 @@ import javax.validation.Valid
 @RequestMapping("/v1/user")
 class UserController {
 
-    @GetMapping
+    @PostMapping
     fun save(
         @RequestBody
         @Valid
         userCandidateRequest: UserCandidateRequest
     ) {
-
+        println(userCandidateRequest)
     }
 }
