8bc9f4771860b08c7b6e255380a314d224320c99
Rafael Martins <rafaeltecinfo9@gmail.com>
2022-04-12 22:19:31 -0300

    Adding jackson-module-kotlin dependency

---
+++ rest/build.gradle.kts
@@ -0,0 +1,9 @@
+dependencies {
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.2")
+
+    constraints {
+        implementation("com.fasterxml.jackson.module:jackson-databind:2.13.2.2") {
+            because("2.13.2 version has CVE-2020-36518")
+        }
+    }
+}
