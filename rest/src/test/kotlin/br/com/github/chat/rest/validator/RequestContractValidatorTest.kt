901539d8eb55730e8f5cbce7a73e904b3e3e6536
Rafael Martins <rafaeltecinfo9@gmail.com>
2022-04-22 16:25:25 -0300

    Implementing request validator

---
+++ rest/src/test/kotlin/br/com/github/chat/rest/validator/RequestContractValidatorTest.kt
@@ -0,0 +1,16 @@
+package br.com.github.chat.rest.validator
+
+import io.kotest.core.spec.style.AnnotationSpec
+import io.kotest.core.spec.style.StringSpec
+
+class RequestContractValidatorTest : AnnotationSpec() {
+
+    private val validator = RequestContractValidator()
+
+    init {
+        @Test
+        fun `should validate the contract of a request`() {
+
+        }
+    }
+}
